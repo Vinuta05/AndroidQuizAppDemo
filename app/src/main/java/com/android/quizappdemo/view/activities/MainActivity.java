b8b45ce6a9b7aa9f3efd104aa14c8e74e9becb8c
V4012088 <vinuta.bhat@saint-gobain.com>
2019-09-11 15:11:55 +0530

    Changes in UI

--- app/src/main/java/com/android/quizappdemo/view/activities/MainActivity.java
+++ app/src/main/java/com/android/quizappdemo/view/activities/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.android.quizappdemo.R;
@@ -21,5 +23,28 @@ public class MainActivity extends AppCompatActivity  {
         ft.add(R.id.frame_id_fragment , frg);
         ft.commit();
     }
+
+
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.txt_exit_app)
+                .setCancelable(false)
+                .setPositiveButton(R.string.dialog_txt_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                })
+                .setNegativeButton(R.string.dialog_text_no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
+    }
+
 
 }
